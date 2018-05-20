de2222cd2abd9f06dcf243e92bff576f33f4cad7
Lyudmyla <glyuda@yahoo.com>
2018-05-20 11:44:46 +0300

    Add work with browser-sync in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@
     var gulp = require('gulp');
     var sass = require('gulp-sass');
     var autoprefixer = require('gulp-autoprefixer');
+		var browserSync = require('browser-sync');
 
 //all paths we use
     var paths = {
@@ -21,7 +22,22 @@
             js: 'public/js'
         }
     };
+
+		//reload function for browserSync
+		var reload = browserSync.reload;
+
 //all tasks we can do
+		gulp.task('browserSync', function() {
+		  browserSync({
+		    server: {
+		      baseDir: "./public/"
+		    },
+		    port: 8080,
+		    open: true,
+		    notify: false
+		  });
+		});
+
     gulp.task('startWork', function() {
         //first load resources
         gulp.start('htmlMove');
@@ -50,32 +66,44 @@
         gulp.watch(paths.src.img, function() {
             gulp.start('imgMove');
         });
+
+				gulp.start('browserSync');
     });
 
+		//move html into public folder & auto-inject into browsers
     gulp.task('htmlMove', function() {
         gulp.src(paths.src.html)
             .pipe(gulp.dest(paths.dest.html))
+						.pipe(reload({stream:true}));
     });
 
+		//compile sass into css & auto-inject into browsers
     gulp.task('cssCreate', function() {
         gulp.src(paths.src.css)
             .pipe(sass())
             .pipe(autoprefixer('last 2 versions'))
-            .pipe(gulp.dest(paths.dest.css));
+            .pipe(gulp.dest(paths.dest.css))
+						.pipe(reload({stream:true}));
     });
 
+		//move vendors css into public folder & auto-inject into browsers
     gulp.task('cssVendorsMove', function() {
         gulp.src(paths.src.css_vendors)
             .pipe(gulp.dest(paths.dest.css))
+						.pipe(reload({stream:true}));
     });
 
+		//mobe javascript into public folder & auto-inject into browsers
     gulp.task('jsMove', function() {
         gulp.src(paths.src.js)
             .pipe(gulp.dest(paths.dest.js))
+						.pipe(reload({stream:true}));
     });
 
+		//move images into public folder & auto-inject into browsers
     gulp.task('imgMove', function() {
         gulp.src(paths.src.img)
             .pipe(gulp.dest(paths.dest.images))
+						.pipe(reload({stream:true}));
     });
 })();
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,12 @@
-/**
- * Created by lyuda on 04.05.2017.
- */
+$(document).ready(function(){
+
+	//set for element with id="wrapper" attribute value for 'display' depending on browser
+	if ($.browser.msie  && parseInt($.browser.version) <= 8) {
+ 		$(function() {$("#wrapper").css("display", "block");
+    			}); 
+	} else {
+ 		$(function() {$("#wrapper").css("display", "none");
+    			});
+	}
+
+}
