0f4f6080da7012abf44201237fa09550a71a458e
Lyudmyla <glyuda@yahoo.com>
2018-05-23 12:49:49 +0300

    Add markup of the header & intro section

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,1 @@
-$(document).ready(function(){
 
-	//set for element with id="wrapper" attribute value for 'display' depending on browser
-	if ($.browser.msie  && parseInt($.browser.version) <= 8) {
- 		$(function() {$("#wrapper").css("display", "block");
-    			}); 
-	} else {
- 		$(function() {$("#wrapper").css("display", "none");
-    			});
-	}
-
-}
