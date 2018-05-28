d4d13e8e4d1d9842535c8dee9a0040fdc91af0f4
Lyudmyla <glyuda@yahoo.com>
2018-05-28 13:53:02 +0300

    Add menu navigation markup

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # 03_markup_portfolio
+
+An adaptive one page portfolio site markup (using of the Grid layout and Flex layout).
+
+Use:
+- HTML\CSS\JavaScript
+- Gulp
+- SCSS
+- Additional fonts
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,15 @@
+const navToggle = document.querySelector('.menu-button');
+const nav = document.querySelector('nav');
+const containerAll = document.querySelector('.container-all');
 
+// change state of our nav menu and underlying page content
+navToggle.addEventListener('click', function() {
+	containerAll.style.transition = 'transform ease-out 250ms';  //just fix some bug for mobile devices with incorrect transition
+	document.body.classList.toggle('nav-is-open');
+})
+
+//hide our nav menu if user click some menu item
+nav.addEventListener('click', function() {
+	containerAll.style.transition = '0ms';  //just fix some bug for mobile devices with incorrect transition
+	document.body.classList.remove('nav-is-open');
+})
